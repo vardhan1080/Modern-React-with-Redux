4e0138646773b9fa760ec3115247dff9cddaba8a
harsha1080 <harsha.erumalla@atos.net>
2024-04-16 16:19:32 +0530

    end

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -4,17 +4,48 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-function App(){
-    return(
+function App() {
+    return (
         <div>
             {/* <img src = {AlexaImage}/>
             <img src = {CortanaImage}/>
             <img src = {SiriImage}/> */}
             {/* <img src = "https://unsplash.com/photos/a-computer-screen-with-a-remote-control-on-it-s5kTY-Ve1c0"/> */}
-            <div>Personal Digital Assitants</div>
-            <ProfileCard title="Alexa" handle="@alexa" image={AlexaImage}/>
-            <ProfileCard title="Cortana" handle="@cortana" image={CortanaImage}/>
-            <ProfileCard title="Siri" handle="@siri" image={SiriImage}/>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assitants</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa"
+                                image={AlexaImage}
+                                description="Alexa is created and owned by Amazon"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana"
+                                image={CortanaImage}
+                                description="Cortana is created and owned by Microsoft"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri"
+                                image={SiriImage}
+                                description="Siri is created and owned by Apple"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
