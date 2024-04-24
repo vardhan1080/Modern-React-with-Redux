fee94ac75811efcd4cbe3ae081341706333aeb9f
harsha1080 <harsha.erumalla@atos.net>
2024-04-24 13:00:42 +0530

    Update Button.js

--- comps/src/Button.js
+++ comps/src/Button.js
@@ -11,12 +11,12 @@ export default function Button({
     outline,
     rounded
 }) {
-    const classes = className('px-3 py-1.5 border',{
-        'border-blue-500 bg-blue-500 text-white':primary,
-        'border-gray-900 bg-gray-900 text-white':secondary,
-        'border-green-500 bg-green-500 text-white':success,
-        'border-yellow-400 bg-yellow-400 text-white':warning,
-        'border-red-500 bg-red-500 text-white':danger
+    const classes = className('px-3 py-1.5 border', {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger
     });
     return <button className={classes}>{children}</button>
 }
