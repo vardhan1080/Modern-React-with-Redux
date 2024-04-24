82943370242436c0d0b9b3272afb10b613df2c6a
harsha1080 <harsha.erumalla@atos.net>
2024-04-24 13:00:02 +0530

    tailwind

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import Button from './Button'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <Button primary>First</Button>
+      </div>
+      <div>
+        <Button secondary>Second</Button>
+      </div>
+      <div>
+        <Button success>Third</Button>
+      </div>
+      <div>
+        <Button warning>Fourth</Button>
+      </div>
+      <div>
+        <Button danger>Fourth</Button>
+      </div>
+    </div>
   )
 }
---
+++ comps/src/Button.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import className from 'classnames';
+
+export default function Button({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) {
+    const classes = className('px-3 py-1.5 border',{
+        'border-blue-500 bg-blue-500 text-white':primary,
+        'border-gray-900 bg-gray-900 text-white':secondary,
+        'border-green-500 bg-green-500 text-white':success,
+        'border-yellow-400 bg-yellow-400 text-white':warning,
+        'border-red-500 bg-red-500 text-white':danger
+    });
+    return <button className={classes}>{children}</button>
+}
+
+Button.propTypes = {
+    checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+        const count = Number(!!primary)
+            + Number(!!secondary)
+            + Number(!!success)
+            + Number(!!warning)
+            + Number(!!danger)
+
+        if (count > 1) {
+            return new Error('Only one of primary/secondary/success/warning/danger can be true');
+        }
+    }
+};
