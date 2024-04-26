7472fcee0eab7ee82d58cc510cb740fe4ed653c3
harsha1080 <harsha.erumalla@atos.net>
2024-04-26 11:22:56 +0530

    Accordion done

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,29 +1,8 @@
-import { GoBell, GoAlert } from 'react-icons/go'
-import Button from './components/Button'
+
 
 export default function App() {
-
-  const handleClick = () => {
-    console.log('button clicked');
-  };
-
-  return (
+  return(
     <div>
-      <div>
-        <Button primary outline onClick={handleClick} className="mb-5"><GoBell />ABCDEFGH</Button>
-      </div>
-      <div>
-        <Button secondary onMouseEnter={handleClick}><GoAlert />ABCDEFGH</Button>
-      </div>
-      <div>
-        <Button success>ABCDEFGH</Button>
-      </div>
-      <div>
-        <Button warning outline rounded>ABCDEFGH</Button>
-      </div>
-      <div>
-        <Button danger>ABCDEFGH</Button>
-      </div>
     </div>
   )
 }
---
+++ comps/src/pages/AccordionPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Accordion from '../components/Accordion';
+
+export default function AccordionPage() {
+
+  const items = [
+    {
+      key:'1',
+      lable:"first",
+      content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    },
+    {
+      key:'2',
+      lable:"second",
+      content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    },
+    {
+      key:'3',
+      lable:"third",
+      content:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+  ];
+
+  return  <Accordion items={items}/>
+}
