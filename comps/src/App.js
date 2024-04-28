a50b325522a499fabbfc1644066ab3f57114c80d
harsha1080 <harsha.erumalla@atos.net>
2024-04-28 22:16:16 +0530

    completed dropdown

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,23 +1,5 @@
-import { useState } from "react"
-import Dropdown from "./components/Dropdown"
-
 export default function App() {
-
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { lable: 'Red', value: 'red' },
-    { lable: 'Green', value: 'green' },
-    { lable: 'Blue', value: 'blue' }
-  ]
-
   return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
+    <div></div>
   )
 }
---
+++ comps/src/pages/DropdownPage.js
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import Dropdown from "../components/Dropdown"
+
+export default function DashboardPage() {
+
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { lable: 'Red', value: 'red' },
+    { lable: 'Green', value: 'green' },
+    { lable: 'Blue', value: 'blue' }
+  ]
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  )
+}
